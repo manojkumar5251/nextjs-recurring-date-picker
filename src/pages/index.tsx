b2a5f6960f3d7f546344a4e6f6ef529feea47dcf
manojkumar5251 <manojkumar5251@gmail.com>
2024-09-10 22:45:25 +0530

    Fixed styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,7 +9,7 @@ dayjs.extend(weekOfYear);
 
 export default function Home() {
   return (
-    <div className="flex flex-col	">
+    <div className="flex flex-col justify-center items-center">
       <h1 className="my-5 font-black">Recurring Date picker</h1>
       <RecurringDatePicker>
         <button className="text-center py-1.5 px-4 text-sm font-medium rounded-lg border border-transparent bg-cyan-500 text-white hover:bg-cyan-600 focus:outline-none focus:bg-cyan-600 disabled:opacity-50 disabled:pointer-events-none">
